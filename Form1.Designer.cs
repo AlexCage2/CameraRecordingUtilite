7df1b433577c542f8679e3762594bf0a7ccfd889
alexcage2 <alexander.kirichenko.o@mail.ru>
2023-10-09 20:51:26 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace AforgeCamereRecording
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.StartRecButton = new System.Windows.Forms.Button();
+            this.StopRecButton = new System.Windows.Forms.Button();
+            this.MainPicture = new System.Windows.Forms.PictureBox();
+            this.DevicesBox = new System.Windows.Forms.ComboBox();
+            this.SelectDeviceButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(286, 344);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(37, 13);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Status";
+            // 
+            // StartRecButton
+            // 
+            this.StartRecButton.Location = new System.Drawing.Point(21, 339);
+            this.StartRecButton.Name = "StartRecButton";
+            this.StartRecButton.Size = new System.Drawing.Size(75, 23);
+            this.StartRecButton.TabIndex = 3;
+            this.StartRecButton.Text = "Start Rec";
+            this.StartRecButton.UseVisualStyleBackColor = true;
+            this.StartRecButton.Click += new System.EventHandler(this.StartRecButton_Click);
+            // 
+            // StopRecButton
+            // 
+            this.StopRecButton.Location = new System.Drawing.Point(112, 339);
+            this.StopRecButton.Name = "StopRecButton";
+            this.StopRecButton.Size = new System.Drawing.Size(75, 23);
+            this.StopRecButton.TabIndex = 4;
+            this.StopRecButton.Text = "Stop Rec";
+            this.StopRecButton.UseVisualStyleBackColor = true;
+            this.StopRecButton.Click += new System.EventHandler(this.StopRecButton_Click);
+            // 
+            // MainPicture
+            // 
+            this.MainPicture.Location = new System.Drawing.Point(12, 12);
+            this.MainPicture.Name = "MainPicture";
+            this.MainPicture.Size = new System.Drawing.Size(776, 321);
+            this.MainPicture.TabIndex = 5;
+            this.MainPicture.TabStop = false;
+            // 
+            // DevicesBox
+            // 
+            this.DevicesBox.FormattingEnabled = true;
+            this.DevicesBox.Location = new System.Drawing.Point(193, 385);
+            this.DevicesBox.Name = "DevicesBox";
+            this.DevicesBox.Size = new System.Drawing.Size(121, 21);
+            this.DevicesBox.TabIndex = 6;
+            // 
+            // SelectDeviceButton
+            // 
+            this.SelectDeviceButton.Location = new System.Drawing.Point(193, 339);
+            this.SelectDeviceButton.Name = "SelectDeviceButton";
+            this.SelectDeviceButton.Size = new System.Drawing.Size(87, 23);
+            this.SelectDeviceButton.TabIndex = 7;
+            this.SelectDeviceButton.Text = "Select Device";
+            this.SelectDeviceButton.UseVisualStyleBackColor = true;
+            this.SelectDeviceButton.Click += new System.EventHandler(this.SelectDeviceButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SelectDeviceButton);
+            this.Controls.Add(this.DevicesBox);
+            this.Controls.Add(this.MainPicture);
+            this.Controls.Add(this.StopRecButton);
+            this.Controls.Add(this.StartRecButton);
+            this.Controls.Add(this.statusLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MainPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button StartRecButton;
+        private System.Windows.Forms.Button StopRecButton;
+        private System.Windows.Forms.PictureBox MainPicture;
+        private System.Windows.Forms.ComboBox DevicesBox;
+        private System.Windows.Forms.Button SelectDeviceButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using Accord.Video.FFMPEG;
+using Accord.Video;
+using Accord.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace AforgeCamereRecording
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private FilterInfoCollection _videoCaptureDevices;
+        private VideoCaptureDevice _finalVideo = null;
+        private VideoCaptureDeviceForm _captureDevice;
+        private Bitmap _video;
+        private VideoFileWriter _fileWriter = new VideoFileWriter();
+        private SaveFileDialog _saveAvi;
+        private DateTime? _isDate;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _videoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            _captureDevice = new VideoCaptureDeviceForm();
+
+            DevicesList();
+
+            if (_captureDevice.ShowDialog(this) == DialogResult.OK)
+            {
+                VideoCaptureDevice videoSource = _captureDevice.VideoDevice;
+                _finalVideo = _captureDevice.VideoDevice;
+                _finalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+                _finalVideo.Start();
+            }
+        }
+
+        private void FinalVideo_NewFrame(object sender, NewFrameEventArgs e)
+        {
+            _video = (Bitmap)e.Frame.Clone();
+            MainPicture.Image = (Bitmap)e.Frame.Clone();
+            if (_fileWriter.IsOpen)
+            {
+                _fileWriter.WriteVideoFrame(_video);
+            }
+        }
+
+        private void StartRecButton_Click(object sender, EventArgs e)
+        {
+            _saveAvi = new SaveFileDialog();
+            _saveAvi.Filter = "Avi Files (*.avi)|*.avi";
+
+            if (_saveAvi.ShowDialog() == DialogResult.OK)
+            {
+                const int frameRate = 8;
+                const int bitRate = 5_000_000;
+                int capturedHeight = _captureDevice.VideoDevice.VideoResolution.FrameSize.Height;
+                int capturedWidth = _captureDevice.VideoDevice.VideoResolution.FrameSize.Width;
+                _fileWriter.Open(_saveAvi.FileName, capturedWidth, capturedHeight, frameRate, VideoCodec.Default, bitRate);
+            }
+
+            statusLabel.Text = "Now Recording";
+        }
+
+        private void StopRecButton_Click(object sender, EventArgs e)
+        {
+            _fileWriter.Close();
+            MainPicture.Image = null;
+            this._finalVideo.Stop();
+            _fileWriter.Close();
+            MainPicture.Image = null;
+            statusLabel.Text = "";
+        }
+
+        private void DevicesList()
+        {
+            if (_videoCaptureDevices.Count < 1)
+            {
+                return;
+            }
+
+            foreach (FilterInfo dev in _videoCaptureDevices)
+            {
+                DevicesBox.Items.Add(dev.Name);
+            }
+
+            DevicesBox.SelectedIndex = 0;
+            _finalVideo = new VideoCaptureDevice(_videoCaptureDevices[DevicesBox.SelectedIndex].MonikerString);
+            _finalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+            _finalVideo.Start();
+        }
+
+        private void SelectDeviceButton_Click(object sender, EventArgs e)
+        {
+            if (_captureDevice.ShowDialog(this) == DialogResult.OK)
+            {   
+                VideoCaptureDevice videoSource = _captureDevice.VideoDevice;
+                _finalVideo = _captureDevice.VideoDevice;
+                _finalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+                _finalVideo.Start();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_finalVideo is null)
+            {
+                return;
+            }
+            if (_finalVideo.IsRunning)
+            {
+                this._finalVideo.Stop();
+                _fileWriter.Close();
+            }
+        }
+    }
+}
